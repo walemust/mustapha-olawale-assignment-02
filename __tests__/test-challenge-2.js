b0467c6060ad5ad4efda77189f0597601c359d96
Olawale Mustapha <walemust28@gmail.com>
2022-07-03 17:15:25 +0100

    initial commit

--- __tests__/test-challenge-2.js
+++ __tests__/test-challenge-2.js
@@ -3,28 +3,13 @@ const assignment = require('../src/assignment.js');
 const { countEvenNumbers } = assignment;
 const maybe = countEvenNumbers === undefined ? test.skip : test;
 
-maybe('Challenge - 2 : my countEvenNumbers function can count even numbers in array of numbers', () => {
-    const numbers = [
-        17,
-        0,
-        67,
-        41,
-        49,
-        21,
-        1,
-        86,
-        76,
-        2,
-        54,
-        14,
-        93,
-        84,
-        14,
-        77,
-        35,
-        7,
-        29,
-        64
-    ]
-    expect(countEvenNumbers(numbers)).toBe(9);
-});
+maybe(
+    'Challenge - 2 : my countEvenNumbers function can count even numbers in array of numbers',
+    () => {
+        const numbers = [
+            17, 0, 67, 41, 49, 21, 1, 86, 76, 2, 54, 14, 93, 84, 14, 77, 35, 7,
+            29, 64
+        ];
+        expect(countEvenNumbers(numbers)).toBe(9);
+    }
+);
